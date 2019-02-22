467f7bee927bdb99b77f33109aa047b6b2f4a035
cafecoder <cafecoder@gmail.com>
2019-02-22 18:40:10 -0500

    Working with lwcdataTable

--- force-app/main/default/lwc/lwcdataTable/lwcdataTable.js
+++ force-app/main/default/lwc/lwcdataTable/lwcdataTable.js
@@ -1,3 +1,40 @@
-import { LightningElement } from 'lwc';
+import { LightningElement ,wire,track} from 'lwc';
+import getAllOpps from '@salesforce/apex/DatatableController.getAllOpps';
 
-export default class LwcdataTable extends LightningElement {}
+export default class LwcdataTable extends LightningElement {
+    @track columns = [{
+            label: 'Opportunity name',
+            fieldName: 'Name',
+            type: 'text',
+            sortable: true
+        },
+        {
+            label: 'Stage Name',
+            fieldName: 'FirstName',
+            type: 'text',
+            sortable: true
+        },
+        {
+            label: 'Close date',
+            fieldName: 'LastName',
+            type: 'text',
+            sortable: true
+        }
+
+    ];
+    @track error; 
+    @track data ;
+    @wire(getAllOpps)
+    wiredOpps({
+        error,
+        data
+    }) {
+        if (data) {
+            this.data = data;
+           // console.log(data);
+           // console.log(JSON.stringify(data, null, '\t'));
+        } else if (error) {
+            this.error = error;
+        }
+    }
+}
